aef6b4097f1dedaa2f8495c3cec8b62e8843c7ca
Chris Mayer <mayerxc@gmail.com>
2017-12-19 08:56:26 -0500

    add in charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,14 +6,25 @@ import Chart from '../components/chart';
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
+        const temps = cityData.list.map( weather => {
+            //kelvin to fahrenheit
+            return ( ( (+weather.main.temp) * (9 / 5) ) - 459.67 );
+        });
         console.log(temps);
+        const pressures = cityData.list.map( weather => {
+            // hPa(millibar or hectopascal) to inHg (inches of of mercury)
+            //return ( (+weather.main.pressure) * 0.02953 );
+            return weather.main.pressure
+        });
+        console.log(pressures);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td>
-                    <Chart data={temps} color="orange" />
-                </td>
+                <td><Chart data={temps} color="orange" units="F" /></td>
+                <td><Chart data={pressures} color="green" units="hPA" /></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
         )
     }
@@ -24,9 +35,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (F)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
